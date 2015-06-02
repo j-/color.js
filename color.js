a637a15964eafd46caed877089a288e7d6419db6
j- <jh@live.com.au>
2015-06-03 00:38:30 +1000

    Added missing parentheses to `Color.getLuminance`

--- color.js
+++ color.js
@@ -831,9 +831,9 @@ Color.getLightness = function (input) {
 Color.getLuminance = function (input) {
 	var arr = Color.getRGBArray(input);
 	var r = arr[R], g = arr[G], b = arr[B];
-	r = (r <= 0.03928) ? (r / 12.92) : pow(r + 0.055 / 1.055, 2.4);
-	g = (g <= 0.03928) ? (g / 12.92) : pow(g + 0.055 / 1.055, 2.4);
-	b = (b <= 0.03928) ? (b / 12.92) : pow(b + 0.055 / 1.055, 2.4);
+	r = (r <= 0.03928) ? (r / 12.92) : pow((r + 0.055) / 1.055, 2.4);
+	g = (g <= 0.03928) ? (g / 12.92) : pow((g + 0.055) / 1.055, 2.4);
+	b = (b <= 0.03928) ? (b / 12.92) : pow((b + 0.055) / 1.055, 2.4);
 	var result = 0.2126 * r + 0.7152 * g + 0.0722 * b;
 	return result;
 };
