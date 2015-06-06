68ecb06cc59a4c4bc37176e4f2b579639c2054ec
j- <jh@live.com.au>
2015-06-06 17:34:16 +1000

    Fixed RebeccaPurple color definition

--- color.js
+++ color.js
@@ -311,7 +311,7 @@ Color.KEYWORDS = {
 	PINK:                   0xffc0cb,
 	PLUM:                   0xdda0dd,
 	POWDERBLUE:             0xb0e0e6,
-	REBECCAPURPLE:          0x993366,
+	REBECCAPURPLE:          0x663399,
 	ROSYBROWN:              0xbc8f8f,
 	ROYALBLUE:              0x4169e1,
 	SADDLEBROWN:            0x8b4513,
